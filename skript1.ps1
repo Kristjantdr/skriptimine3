f613ba776e6ae748f03e8f5a95676dd17a8531a3
Kristjan Teder <kristjan.teder@voco.ee>
2024-11-11 14:43:41 +0200

    using array list

--- skript1.ps1
+++ skript1.ps1
@@ -3,12 +3,12 @@ $array1 = @(1, 2, 3)
 $array2 = @(4, 5, 6)
 
 #define empty result array
-$array3 = @()
+$array3 = New-Objekt -Typename system.collections.arraylist ##
 
 8# add new values to empty array
-$array3 += $array1[0] + $array2[0]
-$array3 += $array1[1] + $array2[1]
-$array3 += $array1[2] + $array2[2]
+$array3.add($array1[0] + $array2[0]) | out-null
+$array3.add($array1[1] + $array2[1]) | out-null
+$array3.add($array1[2] + $array2[2]) | out-null
 
 #output result array
 $array3
