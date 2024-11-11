dad245f6230d685d1a38b5e0306a6f3297e9b960
Kristjan Teder <kristjan.teder@voco.ee>
2024-11-11 15:33:50 +0200

    palgad

--- skript2.ps1
+++ skript2.ps1
@@ -1,12 +1,12 @@
-﻿$Hashtable1 =@{name="John", "Joe", "Mary"; Daysworked=12, 20, 18}
+﻿$Hashtable1 = @{name="John", "Joe", "Mary"; Daysworked=12, 20, 18}
 
-$Hashtable2 =@{name="John", "Joe", "Mary"; Salaryperday=100, 120, 150}
+$Hashtable2 = @{name="John", "Joe", "Mary"; Salaryperday=100, 120, 150}
 
-$Johnsalary =$Hashtable1.Daysworked[0] * $Hashtable2.Salaryperday[0]
-$JoeSalary =$Hashtable1.DaysWorked[1] * $Hashtable2.SalaryPerDay[1]
-$MarySalary =$hashtable1.DaysWorked[2] * $hashtable2.SalaryPerDay[2]
+$Johnsalary = $Hashtable1.Daysworked[0] * $Hashtable2.Salaryperday[0]
+$JoeSalary = $Hashtable1.DaysWorked[1] * $Hashtable2.SalaryPerDay[1]
+$MarySalary = $Hashtable1.DaysWorked[2] * $Hashtable2.SalaryPerDay[2]
 
 
  
-$Hashtable3 =@{name=$Hashtable1.name[0], $Hashtable1.name[1], $Hashtable3.name[2];Salary=$Johnsalary,$JoeSalary, $MarySalary}
+$Hashtable3 = @{name=$Hashtable1.name[0], $Hashtable1.name[1], $Hashtable1.name[2];Salary=$Johnsalary, $JoeSalary, $MarySalary}
 $Hashtable3
