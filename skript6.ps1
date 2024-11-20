a3a7ed41bbdea5e9f07f671c788b223ea2515df9
Kristjan Teder <kristjan.teder@voco.ee>
2024-11-20 11:35:08 +0200

    fail

---
+++ skript6.ps1
@@ -0,0 +1,24 @@
+﻿$data = Import-Csv "C:\Users\Kristjan\skriptimine\data.csv"
+
+$schoolData = @()
+foreach($personData in $data){
+
+$personData.Age = [int]$personData.Age
+$school = ""
+
+    if($personData.Age -ge 4 -and $personData.Age -le 10){
+        $school = "Junior"
+        } elseif($personData.Age -ge 11 -and $personData.Age -le 17){
+        $school = "Senior"
+        }
+        
+
+    $schoolData += ]@{'Name'=$personData.Name; 'School'=$school}
+    $formatteData = $schoolData | ForEach-Object {
+        "$($_.Name),$($_.School)
+}
+
+Set-Content -Path "C:\Users\Kristjan\skriptimine\school_data.csv" -value $formatteData
+
+Write-Host $schoolData
+Export-Csv -NoTypeInformation -Path "C:\Users\Kristjan\skriptimine\school_data.csv"
