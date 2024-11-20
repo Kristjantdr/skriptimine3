866328f1269173d7fbf20175c1c8dae01047da82
Kristjan Teder <kristjan.teder@voco.ee>
2024-11-20 10:50:44 +0200

    pealinnad poolik

---
+++ skript5v2.ps1
@@ -0,0 +1,11 @@
+﻿$countries = @('India', 'Australia', 'China')
+$capitals = @('New Delhi', 'Canberra', 'Beijing')
+
+Write-Host "Select country:" -ForegroundColor Yellow
+for($i = 0; $i -lt $countries.Count; $i++){
+Write-Host ($i + 1)":"$countries[$i] -ForegroundColor Cyan
+}
+
+$selectedCountry = Read-Host "Please select country"
+if($selectedCountry -ge)
+Write-output "$(countries[$selectedCountry-1])'s capital is $(capitals[selectedCountry-1])"
