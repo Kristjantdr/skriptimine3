9efee4d9ad62583fa2c0fc666c99ec4a4e2896d7
Kristjan Teder <kristjan.teder@voco.ee>
2024-11-25 14:18:17 +0200

    Failisuurus

---
+++ skript12.ps1
@@ -0,0 +1,9 @@
+﻿Get-ChildItem -Path "C:\Temp\Test"
+
+Write-Host
+
+$result = Get-ChildItem -Path "C:\Temp\Test" | Where {$_.Name -like "*.csv"} | select Name, Length
+
+Write-Host "Filename: $($result.Name)"
+Write-Host "Size in KB: $($result.Length/1024)"
+Write-Host "Size in MB: $($result.Length/1MB)"
