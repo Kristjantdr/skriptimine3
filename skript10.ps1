f0bfdbbc44f59126730388ab70165e042333d571
Kristjan Teder <kristjan.teder@voco.ee>
2024-11-25 13:33:11 +0200

    Notepad running until count

---
+++ skript10.ps1
@@ -0,0 +1,9 @@
+﻿$step = 0
+do {
+    Write-Host 'Notepad is running' -ForegroundColor Green
+    $step++
+    Start-Sleep -Seconds 1
+    
+    } until((Get-Process | Where{$_.ProcessName -eq 'notepad'}).Count -eq 0)
+   
+   Write-Host $step
