5eda5bbd1c73649e7c4aa7f350a4b9f8c7892aa1
Kristjan Teder <kristjan.teder@voco.ee>
2024-11-25 12:31:29 +0200

    Notepad is running

--- skript8.ps1
+++ skript8.ps1
@@ -1,4 +1,4 @@
 ﻿while(Get-Process | where{$_.ProcessName -eq 'notepad'}){
-    Write-Host 'Notebad is running' -ForegroundColor Green
+    Write-Host 'Notepad is running' -ForegroundColor Green
     Start-Sleep -Seconds 2
     }
