23bfbf378ae6ab3cd66749d1dbb0ad93d3d46e0e
Kristjan Teder <kristjan.teder@voco.ee>
2024-11-25 14:58:22 +0200

    services running

---
+++ skript14.ps1
@@ -0,0 +1,24 @@
+﻿
+function countstart{
+    $total = (Get-Service | Where{$_.Status -eq 'Running'}).Count
+    Write-Host "Total services is running state = $($total)"
+    }
+
+function countstop{
+    $total = (Get-Service | Where{$_.Status -eq 'Stopped'}).Count
+    Write-Host "Total services is running state = $($total)"
+    }
+
+    function countservice{
+    param(
+    [string]$status
+    )
+    $total = (Get-Service | Where{$_.Status -eq $status}).Count
+    Write-Host "Total services in $($status.ToLower()) state = $($total)"
+    }
+
+    countstart
+    countstop
+
+    countservice -status 'Running'
+    countservice('Stopped')
