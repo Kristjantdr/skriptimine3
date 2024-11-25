6d6e023affd62f96768578d6f32a6928a0018166
Kristjan Teder <kristjan.teder@voco.ee>
2024-11-25 13:21:40 +0200

    Notepad running count

---
+++ skript9.ps1
@@ -0,0 +1,9 @@
+﻿$count = 0
+
+while(Get-Process | where{$_.ProcessName -eq 'notepad'}){
+    Write-Host 'Notepad is running' -ForegroundColor Green
+    Start-Sleep -Seconds 1
+    $count++
+    }
+   
+   Write-Host "$count"
